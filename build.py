34a2c3065ee76d109fd846da163ca1e77e548eff
Albert Wigmore <albert@albertwigmore.co.uk>
2017-08-09 18:33:16 +0100

    Bugfix: corrected output from build.py

--- build.py
+++ build.py
@@ -6,8 +6,8 @@ import os
 from settings import team
 
 base_igem = 'http://2017.igem.org/'
-base_team = base_igem + 'Team:Bristol/'
-base_template = base_igem + 'Template:Bristol/'
+base_team = base_igem + 'Team:' + team +'/'
+base_template = base_igem + 'Template:' + team + '/'
 base_raw = '?action=raw&ctype=text/'
 
 extensions = ['.png', '.svg', '.gif', '.jpeg', '.jpg', '.bmp']
